24f00ad4e3dc01f4577e7ebf2557d580ba5e8806
Vivek Hari <vivek.hari@outlook.co.nz>
2017-11-01 23:14:33 +1100

    Set up basic image effect to invert colours.

---
+++ Assets/Scripts/SIVADensityEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SIVADensityEffect : MonoBehaviour
+{
+    public Material EffectMaterial;
+
+    /// <summary>
+    /// OnRenderImage is called after all rendering is complete to render image.
+    /// </summary>
+    /// <param name="src">The source RenderTexture.</param>
+    /// <param name="dest">The destination RenderTexture.</param>
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, EffectMaterial);
+    }
+}
