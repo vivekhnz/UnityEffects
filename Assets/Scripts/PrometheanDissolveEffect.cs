be2852002a5946373525c18d73fb59a352a43d25
Vivek Hari <vivek.hari@outlook.co.nz>
2017-11-15 22:45:46 +1100

    Animate dissolve radius over time.

---
+++ Assets/Scripts/PrometheanDissolveEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrometheanDissolveEffect : MonoBehaviour
+{
+    public float DissolveUnitsPerSecond = 1;
+
+    private Material material;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        material.SetFloat("_DissolveRadius", Time.time * DissolveUnitsPerSecond);
+    }
+}
