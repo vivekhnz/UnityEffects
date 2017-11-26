f3ec5330d8c3d8fca1702d18dc26f8c961eed321
Vivek Hari <vivek.hari@outlook.co.nz>
2017-11-26 15:40:16 +1100

    Spawn particles at mesh dissolve boundaries.

--- Assets/Scripts/PrometheanDissolveEffect.cs
+++ Assets/Scripts/PrometheanDissolveEffect.cs
@@ -4,35 +4,29 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(ParticleSystem))]
 public class PrometheanDissolveEffect : MonoBehaviour
 {
-    const string PARTICLES_SHAPE_MESH = "ShapeModule.m_Mesh";
-    const string PARTICLES_SHAPE_POSITION = "ShapeModule.m_Position";
-
     public float DissolveUnitsPerSecond = 1;
 
     private Material material;
-    private Material particleMaterial;
-    private SerializedObject particlesRef;
+    private ParticleSystem particles;
+    private Collider coll;
 
     private bool isDissolving;
     private Vector3 dissolveOrigin;
     private float dissolveTime = 0;
+    private ParticleSystem.EmitParams emission;
+    private List<Vector4> particleSpawnPoints;
 
     void Start()
     {
         material = GetComponent<MeshRenderer>().material;
-        var mesh = GetComponent<MeshFilter>().sharedMesh;
-
-        var particles = GetComponent<ParticleSystem>();
-        particlesRef = new SerializedObject(particles);
-        particlesRef.FindProperty(PARTICLES_SHAPE_MESH).objectReferenceValue = mesh;
-        particlesRef.ApplyModifiedProperties();
-        particleMaterial = particles.GetComponent<ParticleSystemRenderer>().material;
+        particles = GetComponent<ParticleSystem>();
+        coll = GetComponent<Collider>();
 
         isDissolving = false;
+        particleSpawnPoints = new List<Vector4>();
     }
 
     void Update()
@@ -42,12 +36,35 @@ public class PrometheanDissolveEffect : MonoBehaviour
             float t = Time.time - dissolveTime;
             float radius = t * DissolveUnitsPerSecond;
             material.SetFloat("_DissolveRadius", radius);
-            particleMaterial.SetFloat("_DissolveRadius", radius);
+
+            bool reachedStart = false;
+            for (int i = 0; i < particleSpawnPoints.Count; i++)
+            {
+                var point = particleSpawnPoints[i];
+                if (reachedStart)
+                {
+                    if (point.w > radius + 0.11f)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if (point.w < radius + 0.1)
+                    {
+                        continue;
+                    }
+                    reachedStart = true;
+                    particleSpawnPoints = particleSpawnPoints.Skip(i).ToList();
+                    i = 0;
+                }
+                emission.position = point;
+                particles.Emit(emission, 1);
+            }
         }
         else
         {
             material.SetFloat("_DissolveRadius", 0);
-            particleMaterial.SetFloat("_DissolveRadius", 0);
         }
     }
 
@@ -58,8 +75,21 @@ public class PrometheanDissolveEffect : MonoBehaviour
         dissolveOrigin = origin;
 
         material.SetVector("_DissolveOrigin", dissolveOrigin);
-        particleMaterial.SetVector("_DissolveOrigin", dissolveOrigin);
-        particlesRef.FindProperty(PARTICLES_SHAPE_POSITION).vector3Value =
-            transform.worldToLocalMatrix.MultiplyPoint(dissolveOrigin);
+
+        particleSpawnPoints.Clear();
+        float density = 0.15f;
+        for (float x = coll.bounds.min.x; x < coll.bounds.max.x; x += density)
+        {
+            for (float y = coll.bounds.min.y; y < coll.bounds.max.y; y += density)
+            {
+                for (float z = coll.bounds.min.z; z < coll.bounds.max.z; z += density)
+                {
+                    var point = coll.ClosestPoint(new Vector3(x, y, z));
+                    particleSpawnPoints.Add(new Vector4(point.x, point.y, point.z,
+                        Vector3.Distance(dissolveOrigin, point)));
+                }
+            }
+        }
+        particleSpawnPoints.Sort((a, b) => (int)Mathf.Sign(a.w - b.w));
     }
 }
