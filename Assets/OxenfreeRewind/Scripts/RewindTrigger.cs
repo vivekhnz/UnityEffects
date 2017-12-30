3ca9bbd895343bea2457acc8700f4f6188b89b70
Vivek Hari <vivek.hari@outlook.co.nz>
2017-12-30 20:55:09 +1300

    Added second scene which is navigated to when reaching a trigger.

---
+++ Assets/OxenfreeRewind/Scripts/RewindTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RewindTrigger : MonoBehaviour
+{
+    public string TargetScene = "TargetScene";
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(TargetScene);
+    }
+}
