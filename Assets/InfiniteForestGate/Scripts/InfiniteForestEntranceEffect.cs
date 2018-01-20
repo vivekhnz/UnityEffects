bb5f82ec1bf88dbc688a4eb0d3d50c3548359d97
Vivek Hari <vivek.hari@outlook.co.nz>
2018-01-21 00:20:07 +1300

    Draw grid in non-base triangles.

---
+++ Assets/InfiniteForestGate/Scripts/InfiniteForestEntranceEffect.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
+public class InfiniteForestEntranceEffect : MonoBehaviour
+{
+    void Start()
+    {
+        var filter = GetComponent<MeshFilter>();
+        filter.mesh = CreateMesh();
+    }
+
+    private Mesh CreateMesh()
+    {
+        var mesh = new Mesh();
+
+        // store relative distance to edge in W component
+        var left = new Vector4(-0.5f, -0.5f, 0, 0);  // bottom-left
+        var top = new Vector4(0, 0.5f, 0, 0);        // top
+        var right = new Vector4(0.5f, -0.5f, 0, 0);  // bottom-right
+        var inner = new Vector4(0.0f, -0.2f, 5, 1);  // inside
+
+        var uvLeft = new Vector2(0, 0);
+        var uvRight = new Vector2(1, 0);
+        var uvInner = new Vector2(0.5f, 1);
+
+        // the UVs of each vertex are different for each triangle so we can't share vertices
+        var vertices = new List<Vector4>
+        {
+            left, top, inner,
+            top, right, inner,
+            right, left, inner
+        };
+        var triangles = new List<int>();
+
+        // store UVs and distance to edge in the mesh normals
+        var uvs = new List<Vector2>
+        {
+            uvRight, uvLeft, uvInner,
+            uvRight, uvLeft, uvInner,
+            uvRight, uvLeft, uvInner
+        };
+        var uvDistances = new List<Vector3>();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            triangles.Add(i);
+            var uv = uvs[i];
+            uvDistances.Add(new Vector3(uv.x, uv.y, vertices[i].w));
+        }
+
+        mesh.SetVertices(vertices.Select(v => (Vector3)v).ToList());
+        mesh.SetTriangles(triangles.ToArray(), 0);
+        mesh.SetNormals(uvDistances);
+        return mesh;
+    }
+}
---
+++ Assets/InfiniteForestGate/Shaders/InfiniteForestEntranceShader.shader
@@ -0,0 +1,72 @@
+﻿Shader "Custom/InfiniteForestGate/Entrance"
+{
+	Properties
+	{
+		_GridTexture ("Grid texture", 2D) = "white" {}
+
+		_BaseColor ("Base color", Color) = (0, 0, 0, 1)
+		_UVScrollSpeed ("UV inward scroll speed", Float) = 1
+		_PyramidCutoff ("Pyramid cutoff", Float) = 0.9
+		_GridOpacityRamp ("Grid opacity ramp", Float) = 1
+	}
+
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float3 uv_distance : NORMAL;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float distance : NORMAL;
+			};
+			
+            sampler2D _GridTexture;
+			float4 _GridTexture_ST;
+
+			float3 _BaseColor;
+			float _UVScrollSpeed;
+			float _PyramidCutoff;
+			float _GridOpacityRamp;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+
+				// scroll UVs inward
+				o.uv = TRANSFORM_TEX(v.uv_distance.xy, _GridTexture);
+				o.uv += float2(0, -(_Time.y * _UVScrollSpeed) % 1);
+				
+				o.distance = v.uv_distance.z;
+				return o;
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// cut off grid at center to show central triangle
+				float3 grid = tex2D(_GridTexture, i.uv).rgb;
+				float gridOpacity = step(i.distance, _PyramidCutoff);
+
+				// fade out grid at the outer edges
+				gridOpacity *= pow(i.distance, _GridOpacityRamp);
+				
+				float3 result = _BaseColor + (grid * gridOpacity);
+				return fixed4(result, 1);
+			}
+			ENDCG
+		}
+	}
+}
